6343c7e95744cb85778b3e836741f437fd83122a
Nayeem <moonnayeem87@gmail.com>
2020-07-28 12:11:37 +0600

    Registration Backend & Login

--- SESSION_TRACKING_PHP_TASK_04/registration.php
+++ SESSION_TRACKING_PHP_TASK_04/registration.php
@@ -1,12 +1,22 @@
 <?php
 	if(isset($_POST['submit']))
 	{
-		if($_POST['name'] != "" and $_POST['email'] != "" and $_POST['userName'] != "" and $_POST['password'] != "" and $_POST['confirmPassword'] != "" and isset($_POST['gender']) and $_POST['day'] != "" and $_POST['month'] != "" and $_POST['year'] != "")
+		if($_POST['name'] != "" and $_POST['email'] != "" and $_POST['userName'] != "" and $_POST['password'] != "" and $_POST['confirmPassword'] != "" and $_POST['day'] != "" and isset($_POST['gender']) and $_POST['month'] != "" and $_POST['year'] != "")
 		{
 			if($_POST['password'] == $_POST['confirmPassword'])
 			{
-				
+				$_SESSION['userName']=$_POST['userName'];
+				$_SESSION['password']=$_POST['password'];
+				print_r($_POST);
+				header('location:Login.html');
 			}
+			else
+				header('location:registration.html');
 		}
-	}	
+		else
+			header('location:registration.html');
+	}
+	else
+		header('location:publichome.html');
+
 ?>
