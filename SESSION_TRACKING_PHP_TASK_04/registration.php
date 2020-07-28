e68cdf51ed25b7d485d295e61f0ae38913c513b9
Nayeem <moonnayeem87@gmail.com>
2020-07-28 09:23:31 +0600

    Public Home

---
+++ SESSION_TRACKING_PHP_TASK_04/registration.php
@@ -0,0 +1,12 @@
+<?php
+	if(isset($_POST['submit']))
+	{
+		if($_POST['name'] != "" and $_POST['email'] != "" and $_POST['userName'] != "" and $_POST['password'] != "" and $_POST['confirmPassword'] != "" and isset($_POST['gender']) and $_POST['day'] != "" and $_POST['month'] != "" and $_POST['year'] != "")
+		{
+			if($_POST['password'] == $_POST['confirmPassword'])
+			{
+				
+			}
+		}
+	}	
+?>
