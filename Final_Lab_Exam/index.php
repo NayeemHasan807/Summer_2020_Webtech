716626cb99d497d07a727747e267ec28e22bf9e8
Nayeem <moonnayeem87@gmail.com>
2020-09-06 09:02:36 +0600

    DB,CommonHome & Login

---
+++ Final_Lab_Exam/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+
+	<h1></h1>
+	<a href="views/loginasadmin.php">Login As Admin</a> |
+	<a href="views/loginasauthor.php">Login As Author</a>
+</body>
+</html>
